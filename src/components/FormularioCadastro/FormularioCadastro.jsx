736977f8e413fe37438a7774daf3d113cbf93deb
louis-amorim <louis.samorim@gmail.com>
2021-01-21 08:39:46 -0300

    Tranformando propiedade da classe pai para transmitir os dados da nota

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,28 +2,41 @@ import React, { Component } from "react";
 import "./estilo.css";
 class FormularioCadastro extends Component {
 
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
     this.titulo="";
+    this.texto="";
 
   }
-  handlerMudancaTitulo(evento){
-    this.titulo=evento.target.value
-    console.log(this.titulo)
+  _handlerMudancaTitulo(evento){
+    evento.stopPropagation();
+    this.titulo=evento.target.value;
+  }
+  _handlerMudancaTexto(evento){
+    evento.stopPropagation();
+    this.texto=evento.target.value;
+  }
+  _criarNota(evento){
+    evento.preventDefault();
+    evento.stopPropagation();
+    this.props.criarNota(this.titulo,this.texto);
   }
   render() {
     return (
-      <form className="form-cadastro ">
+      <form className="form-cadastro"
+        onSubmit={this._criarNota.bind(this)}
+      >
         <input
           type="text"
           placeholder="Título"
           className="form-cadastro_input"
-          onChange={this.handlerMudancaTitulo.bind(this)}
+          onChange={this._handlerMudancaTitulo.bind(this)}
         />
         <textarea
           rows={15}
           placeholder="Escreva sua nota..."
           className="form-cadastro_input"
+          onChange={this._handlerMudancaTexto.bind(this)}
         />
         <button className="form-cadastro_input form-cadastro_submit">
           Criar Nota
